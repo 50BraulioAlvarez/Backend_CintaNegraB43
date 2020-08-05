0f518466e30ea5da2f72b62fb411c5f277ff8248
50BraulioAlvarez <braulioalvarezurban@gmail.com>
2020-08-04 20:02:58 -0500

    Cors agregado

--- index.js
+++ index.js
@@ -5,3 +5,4 @@ require('./database');
 app.listen(PORT, (err) => err ? console.error(err) : console.info(`Server on port ${PORT}`))
 
 //https://forums.meteor.com/t/how-to-fix-the-bcrypt-error-in-windows-7/28856/2
+// npm i cors ---> para las peticiones de heroku
--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT;
 const router = require("../routes");
+const cors = require('cors')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use("/api/v1", router);
 
 module.exports = { app, PORT };
